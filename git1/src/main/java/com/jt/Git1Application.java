20c5815869e633c1d886fa0e17ec8743c68c57a3
tonyjiangtb <tonyjiangtb@gmail.com>
2018-03-19 23:07:33 +0800

    4

--- git1/src/main/java/com/jt/Git1Application.java
+++ git1/src/main/java/com/jt/Git1Application.java
@@ -2,7 +2,7 @@ package com.jt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//rb4
 @SpringBootApplication
 public class Git1Application {
 	
